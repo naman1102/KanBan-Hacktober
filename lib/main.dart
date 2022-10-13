89672e3fbc1855340023942dd5d3bbe003273fa8
Naman mehta <51489902+naman1102@users.noreply.github.com>
2022-10-13 21:00:38 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login App',
+      title: 'KanBan App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
